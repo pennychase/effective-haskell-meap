ece66d5077596a3d6c0a9e306d76d1eae091ad84
Penny Chase <mpchase@mac.com>
2022-02-19 22:44:26 -0500

    Fibonacci (Chapter 2)

---
+++ Fibs.hs
@@ -0,0 +1,15 @@
+module Fibs where
+
+fib n 
+    | n == 0 = 0
+    | n == 1 = 1
+    | otherwise = (fib $ n - 1) + (fib $ n - 2)
+
+fibs = map fib [0 ..]
+
+-- Lazy generator for fibonaci
+
+fibs' =
+    0 : 1 : nextFibs fibs (tail fibs)
+    where
+        nextFibs (a:as) (b:bs) = (a+b) : nextFibs as bs
