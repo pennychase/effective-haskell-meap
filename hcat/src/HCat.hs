c77f65726f38fd73d00ddf632e9fefb234931c1d
Penny Chase <mpchase@mac.com>
2022-03-09 18:40:50 -0500

    Chapter 8 - error handling with wothErrorHandling

--- hcat/src/HCat.hs
+++ hcat/src/HCat.hs
@@ -1,14 +1,27 @@
+{-# LANGUAGE LambdaCase #-}
+
 module HCat ( runHCat ) where
 
 import qualified System.Environment as Env
+import qualified Control.Exception as Exception
+import qualified System.IO.Error as IOError
 
+-- Error Handling version 1
+-- Use withErrorHandling to provide a context, while focusing on handleArgs (the real work of runHCat)
 runHCat :: IO ()
-runHCat = handleArgs >>= displayMessage
+runHCat =
+    withErrorHandling $
+        handleArgs
+        >>= \case
+            Left err -> putStrLn $ "Error: " <> err
+            Right fname -> readFile fname >>= putStrLn
     where
-        displayMessage parsedArgument = 
-            case parsedArgument of
-                Left errMsg -> putStrLn $ "Error: " <> errMsg
-                Right fname -> putStrLn $ "Opening file: " <> fname
+        withErrorHandling :: IO () -> IO ()
+        withErrorHandling ioAction = Exception.catch ioAction handleErr
+
+        handleErr :: IOError -> IO ()
+        handleErr e = putStrLn "I ran into an error!" >> print e
+
 
 handleArgs :: IO (Either String FilePath)
 handleArgs = parseArgs <$> Env.getArgs
