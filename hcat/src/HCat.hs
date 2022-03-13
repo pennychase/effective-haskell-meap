29f522c4f7c777a90db36b7c111b028d57515eaf
Penny Chase <mpchase@mac.com>
2022-03-13 19:27:21 -0400

    Finished pur core of hcat (Chapter 8)

--- hcat/src/HCat.hs
+++ hcat/src/HCat.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 
 module HCat ( runHCat ) where
@@ -11,8 +12,7 @@ import qualified System.Environment as Env
 import qualified System.IO.Error as IOError
 
 
--- Error Handling version 2
--- Unify error handling approach by throwing an exception after handle args (turning the Either result into an IO Error)
+-- Top level
 
 runHCat :: IO ()
 runHCat =
@@ -26,7 +26,8 @@ runHCat =
         withErrorHandling ioAction = Exception.catch ioAction $
             \e -> print "I ran into an error!" >> print @IOError e
 
-
+-- Error Handling version 2
+-- Unify error handling approach by throwing an exception after handle args (turning the Either result into an IO Error)
 handleArgs :: IO (Either String FilePath)
 handleArgs = parseArgs <$> Env.getArgs
     where
@@ -39,3 +40,49 @@ handleArgs = parseArgs <$> Env.getArgs
 eitherToErr :: Show a => Either a b -> IO b
 eitherToErr (Right a) = return a
 eitherToErr (Left e) = Exception.throwIO . IOError.userError $ show e
+
+
+-- Pager
+
+-- Group lines of text into pages
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf n [] = []
+groupsOf n elems =
+    let
+        (hd, tl) = splitAt n elems
+    in hd : groupsOf n tl
+
+-- Word wrapping
+wordWrap :: Int -> Text.Text -> [Text.Text]
+wordWrap lineLength lineText
+    | Text.length lineText <= lineLength = [lineText]
+    | otherwise =
+        let
+            (candidate, nextLines) = Text.splitAt lineLength lineText
+            (firstLine, overflow) = softWrap candidate (Text.length candidate - 1)
+        in firstLine : wordWrap lineLength (overflow <> nextLines)
+    where
+        softWrap hardwrappedText textIndex
+            | textIndex <= 0 = (hardwrappedText, Text.empty)
+            | Text.index hardwrappedText textIndex == ' ' = 
+                let
+                    (wrappedLine, rest) = Text.splitAt textIndex hardwrappedText
+                in (wrappedLine, Text.tail rest)
+            | otherwise = softWrap hardwrappedText (textIndex - 1) 
+
+-- Terminal dimensions
+
+data ScreenDimensions = ScreenDimensions
+    { screenRows :: Int 
+    , screenColumns :: Int 
+    } deriving Show
+
+-- Paginate accroding to screen size
+-- Can test with: mapM_ TextIO.putStrLn $ paginate (Screen Dimensions 5 10) "Some ling text ..."
+paginate :: ScreenDimensions -> Text.Text -> [Text.Text]
+paginate (ScreenDimensions rows cols) text =
+    let 
+        unwrappedLines = Text.lines text
+        wrappedLines = concatMap (wordWrap cols) unwrappedLines
+        pageLines = groupsOf rows wrappedLines
+    in map Text.unlines pageLines
